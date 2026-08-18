6b6d1cd3f26075ae3dceac21fe48ce41af6c08ac
Adam's PC <abaer1902@gmail.com>
2026-08-18 16:05:44 -0400

    Changed test to establish a temporary database before testing isolated service

--- Tests/UnitTests/Services/ClientServiceTests.cs
+++ Tests/UnitTests/Services/ClientServiceTests.cs
@@ -1,4 +1,8 @@
 using AvaloniaApplication1.Services;
+using AvaloniaApplication1.Data;
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace UnitTests.Services;
@@ -9,11 +13,32 @@ public class ClientServiceTests
     [Fact]
     public async Task Validate_AddClient()
     {
-        var service = new ClientService();
+        // Create a temporary SQLite database.
+        await using var connection = new SqliteConnection("Data Source=:memory:");
+        await connection.OpenAsync();
 
-        var oldCount = await service.GetClientCountAsync();
-        var result = await service.AddClientAsync("Test", "Smith", "Test.smith@example.com", DateTime.Now);
-        var newCount = await service.GetClientCountAsync();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        await using var context = new AppDbContext(options);
+
+        // Instanciate EF Core tables.
+        await context.Database.EnsureCreatedAsync();
+
+        // Test connection
+        var canConnect = await context.Database.CanConnectAsync();
+        Assert.True(canConnect);
+
+        var directCount = await context.Clients.CountAsync();
+        Assert.Equal(0, directCount);
+
+
+        var service = new ClientService(context);
+
+        var oldCount = await service.GetClientCountAsync();       // -1 if no database pulled
+        var result = await service.AddClientAsync("Test", "Smith", "Test.smith@example.com");
+        var newCount = await service.GetClientCountAsync();       // -1 if no database pulled
 
         Assert.Equal(oldCount + 1, newCount);
     }
